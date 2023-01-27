33df676595f36e7b8ffb264c9488f4368bce4fe9
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-01-27 09:50:29 -0500

    fixed home categories

--- src/App.js
+++ src/App.js
@@ -7,18 +7,30 @@ const App = () => {
           <h3>Toys</h3>
           <p>Shop Now</p>
         </div>
+      </div>
+      <div className="category_container">
+        {/* <img src="" alt="" /> */}
         <div className="category_body__container">
           <h3>Treats</h3>
           <p>Shop Now</p>
         </div>
+      </div>
+      <div className="category_container">
+        {/* <img src="" alt="" /> */}
         <div className="category_body__container">
           <h3>Beds</h3>
           <p>Shop Now</p>
         </div>
+      </div>
+      <div className="category_container">
+        {/* <img src="" alt="" /> */}
         <div className="category_body__container">
           <h3>Leashes</h3>
           <p>Shop Now</p>
         </div>
+      </div>
+      <div className="category_container">
+        {/* <img src="" alt="" /> */}
         <div className="category_body__container">
           <h3>Collars</h3>
           <p>Shop Now</p>
