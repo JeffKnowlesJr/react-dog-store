69215e59b067afa59754871c52ad39ba2131c28e
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-01-27 14:38:44 -0500

    Added React Router Dom and set up first route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import CategoryList from './components/category-list/category-list.component'
+import { Routes, Route } from 'react-router-dom'
+import Home from './routes/home/home.component'
 
 const App = () => {
-  return <CategoryList />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
 }
 
 export default App
