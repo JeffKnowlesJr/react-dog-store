871d9316baa6785fbb62cec0bf8bdd989539b577
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-01-27 11:31:26 -0500

    Added background images

--- src/App.js
+++ src/App.js
@@ -4,32 +4,42 @@ const App = () => {
   const categories = [
     {
       id: 1,
-      title: 'Toys'
+      title: 'Toys',
+      imageURL: './480x320.jpg'
     },
     {
       id: 2,
-      title: 'Treats'
+      title: 'Treats',
+      imageURL: './480x321.jpg'
     },
     {
       id: 3,
-      title: 'Leashes'
+      title: 'Leashes',
+      imageURL: './480x322.jpg'
     },
     {
       id: 4,
-      title: 'Collars'
+      title: 'Collars',
+      imageURL: './800x490.jpg'
     },
     {
       id: 5,
-      title: 'Beds'
+      title: 'Beds',
+      imageURL: './800x491.jpg'
     }
   ]
 
   return (
     <div className="categories__container">
-      {categories.map(({ title, id }) => {
+      {categories.map(({ title, id, imageURL }) => {
         return (
           <div key={id} className="category__container">
-            <div className="category__image"></div>
+            <div
+              className="category__image"
+              style={{
+                backgroundImage: `url(${imageURL})`
+              }}
+            />
             <div className="category-body__container">
               <h3>{title}</h3>
               <p>Shop Now</p>
