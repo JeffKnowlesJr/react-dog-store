ce727644dfe8b4386c5408be4176cae8b42c5282
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-01-27 09:47:12 -0500

    Added Elements to opening screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 const App = () => {
-  return <div className="">Hello World</div>
+  return (
+    <div className="categories_container">
+      <div className="category_container">
+        {/* <img src="" alt="" /> */}
+        <div className="category_body__container">
+          <h3>Toys</h3>
+          <p>Shop Now</p>
+        </div>
+        <div className="category_body__container">
+          <h3>Treats</h3>
+          <p>Shop Now</p>
+        </div>
+        <div className="category_body__container">
+          <h3>Beds</h3>
+          <p>Shop Now</p>
+        </div>
+        <div className="category_body__container">
+          <h3>Leashes</h3>
+          <p>Shop Now</p>
+        </div>
+        <div className="category_body__container">
+          <h3>Collars</h3>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
