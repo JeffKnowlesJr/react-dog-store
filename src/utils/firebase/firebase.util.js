86aaadecd2601e21d6ba6894798e1704bfb0c382
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-03-20 14:43:27 -0400

    Product import complete, then import methods deled

--- src/utils/firebase/firebase.util.js
+++ src/utils/firebase/firebase.util.js
@@ -14,7 +14,15 @@ import {
 } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch
+} from 'firebase/firestore'
+import SHOP_DATA from '../../shop-data'
 
 // Initialize Firebase
 initializeApp(firebaseConfig)
@@ -27,6 +35,24 @@ export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
 
 export const db = getFirestore()
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey)
+  // A transaction is a word that represents a single unit of work to a database
+  // A transaction may include a set of multiple required conditions
+  const batch = writeBatch(db)
+  // Now we need to create the batch
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase())
+    batch.set(docRef, object)
+  })
+
+  await batch.commit()
+  console.log('done')
+}
 
 export const createUserDocumentFromAuth = async (userAuth, userInfo) => {
   if (!userAuth) return
