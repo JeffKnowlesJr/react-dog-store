84d937eb4d0c35bcf7aa1c5439194721917a8b06
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-04-19 19:02:14 -0400

    final commit

--- src/utils/firebase/firebase.util.js
+++ src/utils/firebase/firebase.util.js
@@ -26,13 +26,15 @@ import {
   getDocs
 } from 'firebase/firestore'
 
+require('dotenv').config()
+
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 }
 
 console.log(
