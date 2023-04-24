5916cb4eb2eae7472358404d24048e817c597810
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-04-24 14:03:20 -0400

    Completed user Context reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useEffect, useReducer } from 'react'
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth
@@ -9,15 +9,42 @@ export const UserContext = createContext({
   setCurrentUser: () => null
 })
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, { type, payload }) => {
+  console.log(`Dispatched action: ${type}`)
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER: {
+      return { ...state, currentUser: payload }
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${type} in userReducer`)
+    }
+  }
+}
+
+const INITIAL_STATE = {
+  currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null)
+  // const [currentUser, setCurrentUser] = useState(null)
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE)
+  // console.log(`User Provider Current User: ${JSON.stringify(currentUser)}`)
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+  }
+
   const value = { currentUser, setCurrentUser }
 
   // Empty dependency array means that we only run this once on mount
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) createUserDocumentFromAuth(user)
-      console.log(`User Context: ${JSON.stringify(user)}`)
+      // console.log(`User Context: ${JSON.stringify(user)}`)
       setCurrentUser(user)
     })
     return unsubscribe
